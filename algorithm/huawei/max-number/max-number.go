1a92e3d592b3b35b16d40b8c4f321d65b2d3d651
baal1990 <echooflife123@163.com>
2023-07-09 22:36:49 +0800

    max-number

---
+++ algorithm/huawei/max-number/max-number.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{3, 30, 34, 5, 9}
+	fmt.Println(largestNumber(nums))
+}
+
+func largestNumber(nums []int) string {
+	var strs []string
+	for _,e := nums{
+
+	}
+}
