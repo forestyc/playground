e8972c325358c91ca16127418c0c4d802586058e
Baal19905 <echooflife123@163.com>
2023-08-30 18:18:54 +0800

    play

--- test/main.go
+++ test/main.go
@@ -2,24 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/forestyc/playground/pkg/util"
+	"regexp"
 )
 
 func main() {
-	fmt.Println(util.StandardTime(2024))
-}
+	str := "UC200923   "
 
-func G1(ch chan bool) {
-	var x int
-	go func() { x = 1 }()
-	fmt.Println(x)
-}
-
-func f(id int) {
-	switch id {
-	case 1, 2:
-		fmt.Println("match")
-	default:
-		fmt.Println("not match")
-	}
+	fmt.Println(regexp.MatchString("[a-zA-Z]+\\d{6}", str))
 }
