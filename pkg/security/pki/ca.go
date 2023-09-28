f0577581d714a725e97003f371f95ac9002be040
Baal19905 <echooflife123@163.com>
2023-09-28 17:04:05 +0800

    todo: pki

--- pkg/security/pki/ca.go
+++ pkg/security/pki/ca.go
@@ -1,4 +1,17 @@
 package pki
 
-type CA struct {
+import (
+	"crypto/rand"
+	"crypto/x509"
+)
+
+type Pki struct {
+}
+
+func NewPki() Pki {
+	return Pki{}
+}
+
+func (pki Pki) CreateCertificate() {
+	x509.CreateCertificate(rand.Reader)
 }
