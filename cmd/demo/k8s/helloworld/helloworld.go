983dde6afd046214f761b11d7ad6a6310d0a30f5
Baal19905 <echooflife123@163.com>
2024-02-20 09:12:13 +0800

    liveness-probe

--- cmd/demo/k8s/helloworld/helloworld.go
+++ cmd/demo/k8s/helloworld/helloworld.go
@@ -8,11 +8,19 @@ import (
 func main() {
 	engine := gin.Default()
 	engine.GET("/hello-world", helloWorld())
+	engine.GET("/health", health())
 	engine.Run()
 }
 
 func helloWorld() gin.HandlerFunc {
 	return func(context *gin.Context) {
 		context.JSON(http.StatusOK, "hello world!")
+	}
+}
+
+func health() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		context.Writer.Header().Set("Custom-Header", "Awesome")
+		context.JSON(http.StatusOK, "health")
 	}
 }
