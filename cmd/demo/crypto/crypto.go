6b50f277fc24138b1ec86933b2dacf90e567f61f
forestyc <echooflife123@163.com>
2024-04-12 16:26:28 +0800

    rsa play

---
+++ cmd/demo/crypto/crypto.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"github.com/forestyc/playground/pkg/crypto"
+)
+
+func main() {
+	rsa := crypto.RSA{}
+
+	priv, pub, err := rsa.GenerateKey()
+	if err != nil {
+		panic(err)
+	}
+	plaintext := "daihouda"
+	sum := md5.Sum([]byte(plaintext))
+	fmt.Println(hex.EncodeToString(sum[0:]))
+	sign, err := rsa.SignWithBase64(sum[0:], priv)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("sign:", string(sign))
+	success, err := rsa.VerifyWithBase64(sum[0:], pub, sign)
+	if err != nil {
+		panic(err)
+	}
+	if success == false {
+		fmt.Println("failed")
+	} else {
+		fmt.Println("success")
+	}
+}
