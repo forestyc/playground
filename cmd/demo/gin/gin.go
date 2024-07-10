e6fc5ada4f2b96e34ef737463ec0cbbd6dbc4ff1
forestyc <echooflife123@163.com>
2024-07-10 17:25:56 +0800

    play

--- cmd/demo/gin/gin.go
+++ cmd/demo/gin/gin.go
@@ -3,15 +3,24 @@ package main
 import (
 	"github.com/forestyc/playground/pkg/core/http"
 	"github.com/gin-gonic/gin"
-	netHttp "net/http"
+	"time"
 )
 
 func main() {
 	// ...
 	server := http.NewServer(":8080")
 	server.Serve()
-	server.Router.GET("/", func(c *gin.Context) {
-		c.String(netHttp.StatusOK, "Hello World")
-	})
+	gd := &GinDemo{}
+	server.WithHandler(gd)
+
+	time.Sleep(time.Hour)
 	// ...
+}
+
+type GinDemo struct{}
+
+func (gd *GinDemo) Register(g *gin.Engine) {
+	g.GET("/repayment", func(context *gin.Context) {
+		context.JSON(200, gin.H{"hello": "world"})
+	})
 }
