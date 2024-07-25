6496e243272c1dd5eaaad0d9573349e33971f8e0
forestyc <echooflife123@163.com>
2024-07-25 20:06:35 +0800

    todo

--- cmd/demo/k8s/houseloan/app/entity/db/tables.go
+++ cmd/demo/k8s/houseloan/app/entity/db/tables.go
@@ -2,20 +2,20 @@ package db
 
 import "time"
 
-type Info struct {
-	Id                        *int
+type ConfigInfo struct {
+	Id                        *int `gorm:"primary_key;AUTO_INCREMENT"`
 	Name                      *string
 	BusinessPrincipal         *float64
 	BusinessInterestRate      *float64
 	BusinessPeriods           *int
 	ProvidentFundPrincipal    *float64
 	ProvidentFundInterestRate *float64
-	ProvidentPeriods          *int
+	ProvidentFundPeriods      *int
 	StartDate                 *time.Time
 	CreatedAt                 *time.Time
 	UpdatedAt                 *time.Time
 }
 
-func (t *Info) TableName() string {
-	return "info"
+func (t *ConfigInfo) TableName() string {
+	return "config_info"
 }
--- cmd/demo/k8s/houseloan/app/service/info.go
+++ cmd/demo/k8s/houseloan/app/service/info.go
@@ -3,10 +3,13 @@ package service
 import (
 	"github.com/forestyc/playground/cmd/demo/k8s/houseloan/app/context"
 	"github.com/forestyc/playground/cmd/demo/k8s/houseloan/app/entity/db"
+	"github.com/forestyc/playground/cmd/demo/k8s/houseloan/app/model"
+	"gorm.io/gorm"
 )
 
 type Info struct {
 	ctx *context.Context
+	pi  *PrincipalInterest
 }
 
 func NewInfo(ctx *context.Context) *Info {
@@ -15,8 +18,8 @@ func NewInfo(ctx *context.Context) *Info {
 	}
 }
 
-func (i *Info) Get(id int) ([]db.Info, error) {
-	var info []db.Info
+func (i *Info) Get(id int) ([]db.ConfigInfo, error) {
+	var info []db.ConfigInfo
 	var err error
 	if id > 0 {
 		if err = i.ctx.Db.Session().
@@ -33,4 +36,62 @@ func (i *Info) Get(id int) ([]db.Info, error) {
 		}
 	}
 	return info, nil
+}
+
+func (i *Info) Set(req model.SetInfoReq) error {
+	if req.Id == 0 {
+		return i.create(req)
+	} else {
+		return i.modify(req)
+	}
+}
+
+func (i *Info) create(req model.SetInfoReq) error {
+	entity := db.ConfigInfo{
+		Name:                      &req.Name,
+		BusinessPrincipal:         &req.BusinessPrincipal,
+		BusinessInterestRate:      &req.BusinessInterestRate,
+		BusinessPeriods:           &req.BusinessPeriods,
+		ProvidentFundPrincipal:    &req.ProvidentFundPrincipal,
+		ProvidentFundInterestRate: &req.ProvidentFundInterestRate,
+		ProvidentFundPeriods:      &req.ProvidentFundPeriods,
+		StartDate:                 &req.StartDate,
+	}
+	providentFundLoan := NewPrincipalInterest(
+		req.ProvidentFundPrincipal,
+		req.ProvidentFundInterestRate,
+		req.ProvidentFundPeriods,
+	)
+
+	businessLoan := NewPrincipalInterest(
+		req.ProvidentFundPrincipal,
+		req.ProvidentFundInterestRate,
+		req.ProvidentFundPeriods,
+	)
+
+	db := i.ctx.Db.Session()
+
+	return db.Transaction(func(tx *gorm.DB) error {
+		// save loan info
+		if err := tx.Create(&entity).Error; err != nil {
+			return err
+		}
+		businessLoan.Repayment()
+		return nil
+	})
+}
+
+func (i *Info) modify(req model.SetInfoReq) error {
+	entity := db.ConfigInfo{
+		Id:                        &req.Id,
+		Name:                      &req.Name,
+		BusinessPrincipal:         &req.BusinessPrincipal,
+		BusinessInterestRate:      &req.BusinessInterestRate,
+		BusinessPeriods:           &req.BusinessPeriods,
+		ProvidentFundPrincipal:    &req.ProvidentFundPrincipal,
+		ProvidentFundInterestRate: &req.ProvidentFundInterestRate,
+		ProvidentFundPeriods:      &req.ProvidentFundPeriods,
+		StartDate:                 &req.StartDate,
+	}
+	return i.ctx.Db.Session().Updates(&entity).Error
 }
