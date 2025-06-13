12992527c374a1d6cab8b1e52cfd23c8d8a96891
forestyc <echooflife123@163.com>
2025-06-13 17:31:45 +0800

    feat: crypto-tool

---
+++ cmd/demo/crypto-tool/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/forestyc/playground/pkg/security/crypto"
+)
+
+func main() {
+	var text, method, cipher, plain string
+	var err error
+	key := []byte("f9718298fcae5859")
+	flag.StringVar(&text, "text", "", "Cipher text/Plain text")
+	flag.StringVar(&method, "method", "", "encrypt/decrypt")
+	flag.Parse()
+	sm4 := crypto.SM4{}
+	switch method {
+	case "encrypt":
+		plain = text
+		if cipher, err = sm4.EncryptWithBase64([]byte(plain), key); err != nil {
+			panic(err)
+		}
+	case "decrypt":
+		cipher = text
+		var plainByte []byte
+		if plainByte, err = sm4.DecryptWithBase64(cipher, key); err != nil {
+			panic(err)
+		}
+		plain = string(plainByte)
+	default:
+		panic("Unkonwn method")
+	}
+	fmt.Printf("Plain text: [%s]\n", plain)
+	fmt.Printf("Cipher text: [%s]\n", cipher)
+}
